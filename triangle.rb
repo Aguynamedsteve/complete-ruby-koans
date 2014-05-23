fa8a90969a14a573fa4e3e6824bf33f70a937bce
Steve <steve.barnesjr@yahoo.com>
2014-05-22 23:44:05 -0700

    true or false

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,12 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  #if a == b && b == c
+  #  :equilateral
+  #elsif a == b || c
+  #  :isosceles
+  #else a != b || c && b != c
+  #  :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
