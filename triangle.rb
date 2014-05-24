eead3a6aeb2060f26f09934b0d9139425320c2dc
Steve <steve.barnesjr@yahoo.com>
2014-05-23 23:45:00 -0700

    triangles 1

--- triangle.rb
+++ triangle.rb
@@ -14,12 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  #if a == b && b == c
-  #  :equilateral
-  #elsif a == b || c
-  #  :isosceles
-  #else a != b || c && b != c
-  #  :scalene
+  if a == b && b == c && a == c
+    return :equilateral
+  elsif (a == b) || (a == c) || (b == c) 
+    return :isosceles
+  else 
+    return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
